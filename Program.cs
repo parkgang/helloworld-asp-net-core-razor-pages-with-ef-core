d0b7a59a9ef4ec3d92dd6155da4855021ee5694e
박경은 <ruddms936@naver.com>
2021-08-23 10:43:00 +0900

    add: 데이터베이스 만들기

--- Program.cs
+++ Program.cs
@@ -1,5 +1,9 @@
+using HelloworldAspNetCoreRazorPagesWithEfCore.Data;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
 
 namespace HelloworldAspNetCoreRazorPagesWithEfCore
 {
@@ -7,7 +11,28 @@ namespace HelloworldAspNetCoreRazorPagesWithEfCore
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            CreateDbIfNotExists(host);
+
+            host.Run();
+        }
+
+        private static void CreateDbIfNotExists(IHost host)
+        {
+            using var scope = host.Services.CreateScope();
+            var services = scope.ServiceProvider;
+            try
+            {
+                var context = services.GetRequiredService<SchoolContext>();
+                context.Database.EnsureCreated();
+                // DbInitializer.Initialize(context);
+            }
+            catch (Exception ex)
+            {
+                var logger = services.GetRequiredService<ILogger<Program>>();
+                logger.LogError(ex, "An error occurred creating the DB.");
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
