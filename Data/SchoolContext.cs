95106689010ec44978512a8538a3fe783fe81808
박경은 <ruddms936@naver.com>
2021-08-23 10:35:38 +0900

    add: 데이터베이스 컨텍스트 클래스 업데이트

--- Data/SchoolContext.cs
+++ Data/SchoolContext.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using HelloworldAspNetCoreRazorPagesWithEfCore.Models;
 
 namespace HelloworldAspNetCoreRazorPagesWithEfCore.Data
 {
@@ -8,6 +9,18 @@ namespace HelloworldAspNetCoreRazorPagesWithEfCore.Data
         {
         }
 
-        public DbSet<HelloworldAspNetCoreRazorPagesWithEfCore.Models.Student> Student { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        /// <summary>
+        /// SchoolContext가 초기화되었을 때 모델이 잠기고 컨텍스트를 초기화하는 데 사용되기 전에 호출됩니다.
+        /// </summary>
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>().ToTable("Course");
+            modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
+            modelBuilder.Entity<Student>().ToTable("Student");
+        }
     }
 }
